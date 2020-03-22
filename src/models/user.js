fe46cc0164b8375c432e8877717e617701ed6130
Vaibhav Negi <10vaibhavsinghnegi@gmail.com>
2020-03-22 19:18:32 +0530

    worked on authentication

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-
-///////////////////////////////////////////////////// creating model    
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -11,6 +11,7 @@ const User = mongoose.model('User', {
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim:true,
         lowercase: true,
@@ -39,7 +40,47 @@ const User = mongoose.model('User', {
                 throw new Error("Age must be positive.")
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            require:true
+        }
+    }]
 })
+
+// creating token function for instance
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+
+    const token = await jwt.sign({ _id: user._id.toString()}, 'thisismeblackmask')
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+    return token
+}
+
+// creating login function for model
+userSchema.statics.findByCredentials = async (email,password) => {
+    const user = await User.findOne({email})
+    if (!user) {
+        throw new Error('Unable to login!')
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+        throw new Error('Unable to login!')
+    }
+    return user
+}
+
+// creating schema
+userSchema.pre('save', async function (next) {
+    const user = this
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password , 8)
+    }
+    next()
+})
+ 
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
--- src/router/user.js
+++ src/router/user.js
@@ -8,9 +8,20 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({user,token})
     } catch (error) {
         res.status(400).send(error)
+    }
+})
+
+router.post('/users/login', async (req,res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email,req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({user,token})
+    } catch (error) {
+        res.status(400).send()
     }
 })
 
@@ -49,10 +60,15 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        })
+
+        const user = await User.findById(req.params.id)
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
+        
+        // const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+        //     new: true,
+        //     runValidators: true
+        // })
         if (!user) {
             return res.status(404).send()
         }
